c8d0b0a0baf6758a648c00571555f290b3004748
Ketevan Gogia <91284247+okrecommendation9394@users.noreply.github.com>
2022-06-06 00:34:42 -0700

    feat: changes in js

--- pages/main.js
+++ pages/main.js
@@ -1,14 +1,34 @@
+const mainBody=document.querySelector('.body')
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector('.nav-menu');
+let layout = document.createElement('div');
+layout.classList.add('layout');
 
 hamburger.addEventListener('click',()=>{
     hamburger.classList.toggle('active');
     navMenu.classList.toggle('active');
     document.querySelector('.nav-menu').style.display='flex';
     document.querySelector('.nav-menu').style.width='285px';
+    mainBody.appendChild(layout);
 
+   /* hamburger.addEventListener('click',()=>{
+        //  layout.classList.remove('layout');
+          mainBody.removeChild(layout);
+      })*/
 })
 document.querySelectorAll('.nav-link').forEach(n => n.addEventListener('click', ()=>{
     hamburger.classList.remove('active');
     navMenu.classList.remove('active');
 }));
+let arr=[];
+fetch('pets.json').then(function(response){
+    return response.json();
+}).then(function (obj){
+    console.log(obj);
+    document.getElementById('katrine').addEventListener('click', ()=>{
+        mainBody.appendChild(layout);
+    })
+}).catch(function (error){
+    console.error('Error');
+     console.error(error);
+})
